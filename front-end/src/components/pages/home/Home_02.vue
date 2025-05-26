7154c1fa9a1711ab4f5227d5b623b42bce383ff6
ksyou <rnjstn819@gmail.com>
2025-05-26 16:56:14 +0900

    style: 메뉴 아이콘 hover 스타일 추가 및 마이페이지 닉네임 출력으로 변경

--- front-end/src/components/pages/home/Home_02.vue
+++ front-end/src/components/pages/home/Home_02.vue
@@ -7,13 +7,13 @@
     </div>
   </section>
 
-  <section class="w-fit mx-auto">
+  <section class="w-fit max-w-[90%] mx-auto">
     <!-- 프로필 부분 -->
     <div class="flex justify-between items-center mt-10">
       <div class="flex gap-3 items-center">
         <ProfileIcon />
         <div>
-          <h3 class="h3 fw-black">{{ userData?.username ?? '사용자' }}님은</h3>
+          <h3 class="h3 fw-black">{{ userData?.nickname ?? '사용자' }}님은</h3>
           <h4 class="h4">
             <span class="h4 fw-black text-gray-600">Silver</span> 등급입니다.
           </h4>
@@ -129,7 +129,7 @@ const router = useRouter();
 
 const userStore = useUserStore();
 const { user } = storeToRefs(userStore);
-const userData = computed(() => user.value?.data);
+const userData = computed(() => user.value);
 
 onMounted(() => {
   if (!user.value) userStore.fetchUser();
